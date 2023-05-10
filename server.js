1687612b4caf416370665e787ad64b4811b1a873
Verman8555 <83014894+Verman8555@users.noreply.github.com>
2023-05-10 16:49:42 +0530

    requesting raw weather data

--- server.js
+++ server.js
@@ -3,8 +3,23 @@ const request = require("request")
 const bodyParser = require("body-parser")
 const app = express();
 
+require("dotenv").config();
+
+let apiKey = `${process.env.ApiKey}`;
+let city = 'Patna';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+const data = request(url, (err,res,body) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(body);
+    }
+});
+
 app.get('/',(req,res) => {
-    res.send("Hello")
+    res.send(data);
 })
 
 app.listen(3000, () => {

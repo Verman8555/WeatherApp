a3f9f9d13715f2086f83d1008537b8e118a1172e
Verman8555 <83014894+Verman8555@users.noreply.github.com>
2023-05-10 19:56:07 +0530

    adding front page with style

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 require("dotenv").config();
 
+app.use(express.static('public'));
+app.set("view engine","ejs");
+
 let apiKey = `${process.env.ApiKey}`;
 let city = 'Patna';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
@@ -14,12 +17,15 @@ const data = request(url, (err,res,body) => {
         console.log(err);
     }
     else{
-        console.log(body);
+        let weather = JSON.parse(body);
+        //console.log(weather);
+        let msg = `The temperature in ${city} is ${weather.main.temp} F`;
+        console.log(msg);
     }
 });
 
 app.get('/',(req,res) => {
-    res.send(data);
+    res.render('index');
 })
 
 app.listen(3000, () => {
